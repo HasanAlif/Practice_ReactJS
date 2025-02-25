7d4e7d4c492d47d51bf25e762a35e00c10d8001e
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-02-25 15:45:49 +0600

    Counter

--- first_app/src/App.jsx
+++ first_app/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
+import Counter from "../Counter.jsx";
 
 function App() {
     return (
         <>
             <h1>BlockBuster Deals , Shop Now</h1>
             <ProductTab />
+            <h1>States in React</h1>
+            <Counter />
         </>
     );
 }
