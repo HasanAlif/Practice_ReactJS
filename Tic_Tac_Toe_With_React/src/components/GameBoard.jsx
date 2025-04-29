1a271bea117f4352bf931bddaf846b91e9947894
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-04-30 03:48:58 +0600

    Added Game Logic

--- Tic_Tac_Toe_With_React/src/components/GameBoard.jsx
+++ Tic_Tac_Toe_With_React/src/components/GameBoard.jsx
@@ -1,39 +1,19 @@
 
-
-const initialGameBoard = [
-  ["", "", ""],
-  ["", "", ""], 
-  ["", "", ""],
-];
-
-export default function GameBoard({onSelectSquare, turns}) {
-  let gameBoard = initialGameBoard;
-
-  for(const turn of turns){
-    const {square, Player} = turn;
-    const {row, col} = square;
-    gameBoard[row][col] = Player;
-  }
-
-    // const[gameBoard,setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex){
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //     onSelectSquare();
-    // }
-
+export default function GameBoard({ onSelectSquare, board }) {
+  
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== ""}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
