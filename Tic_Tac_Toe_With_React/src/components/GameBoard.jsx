1809eb6ea7edc30235170261834da01879da4157
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-04-30 01:41:47 +0600

    Update GameBoard Functionality

--- Tic_Tac_Toe_With_React/src/components/GameBoard.jsx
+++ Tic_Tac_Toe_With_React/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 
 const initialGameBoard = [
   ["", "", ""],
@@ -6,17 +6,25 @@ const initialGameBoard = [
   ["", "", ""],
 ];
 
-export default function GameBoard({onSelectSquare}) {
-    const[gameBoard,setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns}) {
+  let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex){
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = "X";
-            return updatedBoard;
-        });
-        onSelectSquare();
-    }
+  for(const turn of turns){
+    const {square, Player} = turn;
+    const {row, col} = square;
+    gameBoard[row][col] = Player;
+  }
+
+    // const[gameBoard,setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex){
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectSquare();
+    // }
 
   return (
     <ol id="game-board">
@@ -25,7 +33,7 @@ export default function GameBoard({onSelectSquare}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
